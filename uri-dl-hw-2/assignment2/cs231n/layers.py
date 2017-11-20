b713b4fe7e04bf1432f10073eaea89f0439acc42
arasdar <aras.dargazany@gmail.com>
2017-11-20 08:38:40 -0500

    uri-dl-hw2-convnet

--- uri-dl-hw-2/assignment2/cs231n/layers.py
+++ uri-dl-hw-2/assignment2/cs231n/layers.py
@@ -406,115 +406,6 @@ def dropout_backward(dout, cache):
         dx = dout
     return dx
 
-def conv_forward_naive(x, w, b, conv_param):
-    """
-    A naive implementation of the forward pass for a convolutional layer.
-
-    The input consists of N data points, each with C channels, height H and
-    width W. We convolve each input with F different filters, where each filter
-    spans all C channels and has height HH and width HH.
-
-    Input:
-    - x: Input data of shape (N, C, H, W)
-    - w: Filter weights of shape (F, C, HH, WW)
-    - b: Biases, of shape (F,)
-    - conv_param: A dictionary with the following keys:
-      - 'stride': The number of pixels between adjacent receptive fields in the
-        horizontal and vertical directions.
-      - 'pad': The number of pixels that will be used to zero-pad the input.
-
-    Returns a tuple of:
-    - out: Output data, of shape (N, F, H', W') where H' and W' are given by
-      H' = 1 + (H + 2 * pad - HH) / stride
-      W' = 1 + (W + 2 * pad - WW) / stride
-    - cache: (x, w, b, conv_param)
-    """
-    out = None
-    ###########################################################################
-    # TODO: Implement the convolutional forward pass.                         #
-    # Hint: you can use the function np.pad for padding.                      #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
-    cache = (x, w, b, conv_param)
-    return out, cache
-
-
-def conv_backward_naive(dout, cache):
-    """
-    A naive implementation of the backward pass for a convolutional layer.
-
-    Inputs:
-    - dout: Upstream derivatives.
-    - cache: A tuple of (x, w, b, conv_param) as in conv_forward_naive
-
-    Returns a tuple of:
-    - dx: Gradient with respect to x
-    - dw: Gradient with respect to w
-    - db: Gradient with respect to b
-    """
-    dx, dw, db = None, None, None
-    ###########################################################################
-    # TODO: Implement the convolutional backward pass.                        #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
-    return dx, dw, db
-
-
-def max_pool_forward_naive(x, pool_param):
-    """
-    A naive implementation of the forward pass for a max pooling layer.
-
-    Inputs:
-    - x: Input data, of shape (N, C, H, W)
-    - pool_param: dictionary with the following keys:
-      - 'pool_height': The height of each pooling region
-      - 'pool_width': The width of each pooling region
-      - 'stride': The distance between adjacent pooling regions
-
-    Returns a tuple of:
-    - out: Output data
-    - cache: (x, pool_param)
-    """
-    out = None
-    ###########################################################################
-    # TODO: Implement the max pooling forward pass                            #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
-    cache = (x, pool_param)
-    return out, cache
-
-
-def max_pool_backward_naive(dout, cache):
-    """
-    A naive implementation of the backward pass for a max pooling layer.
-
-    Inputs:
-    - dout: Upstream derivatives
-    - cache: A tuple of (x, pool_param) as in the forward pass.
-
-    Returns:
-    - dx: Gradient with respect to x
-    """
-    dx = None
-    ###########################################################################
-    # TODO: Implement the max pooling backward pass                           #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
-    return dx
-
-
 def spatial_batchnorm_forward(x, gamma, beta, bn_param):
     """
     Computes the forward pass for spatial batch normalization.
@@ -635,3 +526,296 @@ def softmax_loss(x, y):
     dx[np.arange(N), y] -= 1
     dx /= N
     return loss, dx
+
+def get_im2col_indices(X_shape, field_height, field_width, padding=1, stride=1):
+    # First figure out what the size of the output should be
+    # Input shape
+    N, C, H, W = X_shape
+    
+    # Kernel shape
+    # field_height, field_width = kernel_shape
+    field_C = C
+    
+    # Output shape
+    assert (H + (2 * padding) - field_height) % stride == 0
+    assert (W + (2 * padding) - field_width) % stride == 0
+    out_height = int(((H + (2 * padding) - field_height) / stride) + 1)
+    out_width = int(((W + (2 * padding) - field_width) / stride) + 1)
+    out_C = 1 # the output channel/ depth
+
+    # Row, Height, i
+    i0 = np.repeat(np.arange(field_height), field_width)
+    i0 = np.tile(i0, field_C)
+    i1 = np.repeat(np.arange(out_height), out_width)
+    i1 = np.tile(i1, out_C)
+    i1 *= stride
+    
+    # Column, Width, j
+    j0 = np.tile(np.arange(field_width), field_height * field_C)
+    j1 = np.tile(np.arange(out_width), out_height * out_C)
+    j1 *= stride
+    
+    # Channel, Depth, K
+    k0 = np.repeat(np.arange(field_C), field_height * field_width) #.reshape(-1, 1) # out_C = 1
+    k1 = np.repeat(np.arange(out_C), out_height * out_width) #.reshape(-1, 1) # out_C = 1
+    k1 *= stride
+    
+    # Indices: i, j, k index
+    i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+    j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+    k = k0.reshape(-1, 1) + k1.reshape(1, -1)
+    
+    return (k.astype(int), i.astype(int), j.astype(int))
+
+def im2col_indices(X, field_height, field_width, padding=1, stride=1):
+    """ An implementation of im2col based on some fancy indexing """
+    # Zero-pad the input
+    p = padding
+    X_padded = np.pad(X, ((0, 0), (0, 0), (p, p), (p, p)), mode='constant') # X_NxCxHxW
+
+    k, i, j = get_im2col_indices(X.shape, field_height, field_width, padding, stride)
+
+    X_col = X_padded[:, k, i, j] # X_col_txkxn
+    
+    N, C, H, W = X.shape
+    
+    # field_height, field_width = kernel_shape
+    field_C = C # x.shape[1]
+    kernel_size = field_C * field_height * field_width
+    
+    X_col = X_col.transpose(1, 2, 0).reshape(kernel_size, -1)
+    
+    return X_col
+
+def col2im_indices(X_col, X_shape, field_height=3, field_width=3, padding=1, stride=1):
+    """ An implementation of col2im based on fancy indexing and np.add.at """
+    N, C, H, W = X_shape
+    H_padded, W_padded = H + (2 * padding), W + (2 * padding)
+    X_padded = np.zeros((N, C, H_padded, W_padded), dtype=X_col.dtype)
+    
+    k, i, j = get_im2col_indices(X_shape, field_height, field_width, padding, stride)
+
+    # field_height, field_width = kernel_shape
+    field_C = C # x.shape[1]
+    kernel_size = field_C * field_height * field_width
+
+    X_col = X_col.reshape(kernel_size, -1, N).transpose(2, 0, 1) # N, K, H * W
+    np.add.at(X_padded, (slice(None), k, i, j), X_col) # slice(None)== ':'
+    # # np.add.at(a, (slice(None), 0, 1, 2), cols_reshaped) # slice(None)== ':'
+    
+    if padding > 0:
+        X = X_padded[:, :, padding:-padding, padding:-padding]
+    else:
+        X = X_padded[:, :, :, :]
+        
+    return X
+
+def conv_forward_naive(x, w, b, conv_param):
+    """
+    A naive implementation of the forward pass for a convolutional layer.
+
+    The input consists of N data points, each with C channels, height H and
+    width W. We convolve each input with F different filters, where each filter
+    spans all C channels and has height HH and width HH.
+
+    Input:
+    - x: Input data of shape (N, C, H, W)
+    - w: Filter weights of shape (F, C, HH, WW)
+    - b: Biases, of shape (F,)
+    - conv_param: A dictionary with the following keys:
+      - 'stride': The number of pixels between adjacent receptive fields in the
+        horizontal and vertical directions.
+      - 'pad': The number of pixels that will be used to zero-pad the input.
+
+    Returns a tuple of:
+    - out: Output data, of shape (N, F, H', W') where H' and W' are given by
+      H' = 1 + (H + 2 * pad - HH) / stride
+      W' = 1 + (W + 2 * pad - WW) / stride
+    - cache: (x, w, b, conv_param)
+    """
+    out = None
+    ###########################################################################
+    # TODO: Implement the convolutional forward pass.                         #
+    # Hint: you can use the function np.pad for padding.                      #
+    ###########################################################################    
+    # Input X
+    n_x, d_x, h_x, w_x = x.shape
+    
+    # Kernel W
+    n_filter, d_filter, h_filter, w_filter = w.shape
+    
+    # Conv parameters
+    stride, padding = conv_param['stride'], conv_param['pad']
+    
+    # Output
+    h_out = ((h_x + (2 * padding) - h_filter) / stride) + 1
+    w_out = ((w_x + (2 * padding) - w_filter) / stride) + 1
+
+    # Make sure the resukting h_out and w_out are integers
+    if not h_out.is_integer() or not w_out.is_integer():
+        raise Exception('Invalid output dimension!')
+
+    # Type cast the resulting h and w output size
+    h_out, w_out = int(h_out), int(w_out)
+
+    # Changing convolution to matrix multiplication with im2col
+    X_col = im2col_indices(X=x, field_height=h_filter, field_width=w_filter, padding=padding, stride=stride)
+    W_col = w.reshape(n_filter, -1)
+
+    out = (W_col @ X_col) + b.reshape(-1, 1)
+    out = out.reshape(n_filter, h_out, w_out, n_x).transpose(3, 0, 1, 2)
+
+    #     TODO
+    #     # cache = (X, W, b, stride, padding, X_col)
+    #     conv_param = stride, padding, X_col
+
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    cache = (x, w, b, conv_param)
+    return out, cache
+
+def conv_backward_naive(dout, cache):
+    """
+    A naive implementation of the backward pass for a convolutional layer.
+
+    Inputs:
+    - dout: Upstream derivatives.
+    - cache: A tuple of (x, w, b, conv_param) as in conv_forward_naive
+
+    Returns a tuple of:
+    - dx: Gradient with respect to x
+    - dw: Gradient with respect to w
+    - db: Gradient with respect to b
+    """
+    dx, dw, db = None, None, None
+    ###########################################################################
+    # TODO: Implement the convolutional backward pass.                        #
+    ###########################################################################
+    #     x, w, b, stride, padding, X_col = cache
+    x, w, b, conv_param = cache
+    stride, padding = conv_param['stride'], conv_param['pad']
+    n_filter, d_filter, h_filter, w_filter = w.shape
+
+    db = np.sum(dout, axis=(0, 2, 3))
+    db = db.reshape(n_filter, -1)
+
+    dout = dout.transpose(1, 2, 3, 0).reshape(n_filter, -1)
+    
+    # Changing convolution to matrix multiplication with im2col
+    X_col = im2col_indices(X=x, field_height=h_filter, field_width=w_filter, padding=padding, stride=stride)
+    dw = dout @ X_col.T
+    dw = dw.reshape(w.shape)
+
+    w = w.reshape(n_filter, -1)
+    dX_col = w.T @ dout
+    dx = col2im_indices(X_col=dX_col, X_shape=x.shape, field_height=h_filter, field_width=w_filter, 
+                        padding=padding, stride=stride)
+
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return dx, dw, db
+
+def max_pool_forward_naive(x, pool_param):
+    """
+    A naive implementation of the forward pass for a max pooling layer.
+
+    Inputs:
+    - x: Input data, of shape (N, C, H, W)
+    - pool_param: dictionary with the following keys:
+      - 'pool_height': The height of each pooling region
+      - 'pool_width': The width of each pooling region
+      - 'stride': The distance between adjacent pooling regions
+
+    Returns a tuple of:
+    - out: Output data
+    - cache: (x, pool_param)
+    """
+    out = None
+    ###########################################################################
+    # TODO: Implement the max pooling forward pass                            #
+    ###########################################################################
+    #     def _pool_forward(X, pool_fun, size=2, stride=2):
+    n, d, h, w = x.shape
+    pool_height, pool_width, stride = pool_param['pool_height'], pool_param['pool_width'], pool_param['stride']
+    
+    #     h_out = (h - size) / stride + 1
+    #     w_out = (w - size) / stride + 1
+    h_out = (h - pool_height) / stride + 1
+    w_out = (w - pool_width) / stride + 1
+
+    if not w_out.is_integer() or not h_out.is_integer():
+        raise Exception('Invalid output dimension!')
+
+    h_out, w_out = int(h_out), int(w_out)
+
+    X_reshaped = x.reshape(n * d, 1, h, w)
+    X_col = im2col_indices(X=X_reshaped, field_height=pool_height, field_width=pool_width, 
+                           padding=0, stride=stride)
+
+    #     out, pool_cache = pool_fun(X_col)
+    #     def maxpool(X_col):
+    max_idx = np.argmax(X_col, axis=0)
+    out = X_col[max_idx, range(max_idx.size)]
+    #         return out, max_idx
+    #     out, _ = maxpool(X_col)
+    
+    out = out.reshape(h_out, w_out, n, d)
+    out = out.transpose(2, 3, 0, 1)
+
+    #     cache = (X, size, stride, X_col, pool_cache)
+    #     return out, cache
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    cache = (x, pool_param)
+    return out, cache
+
+
+def max_pool_backward_naive(dout, cache):
+    """
+    A naive implementation of the backward pass for a max pooling layer.
+
+    Inputs:
+    - dout: Upstream derivatives
+    - cache: A tuple of (x, pool_param) as in the forward pass.
+
+    Returns:
+    - dx: Gradient with respect to x
+    """
+    dx = None
+    ###########################################################################
+    # TODO: Implement the max pooling backward pass                           #
+    ###########################################################################
+    # def _pool_backward(dout, dpool_fun, cache):
+    x, pool_param = cache
+    n, d, w, h = x.shape
+    pool_height, pool_width, stride = pool_param['pool_height'], pool_param['pool_width'], pool_param['stride']    
+
+    X_reshaped = x.reshape(n * d, 1, h, w)
+    X_col = im2col_indices(X=X_reshaped, field_height=pool_height, field_width=pool_width, 
+                           padding=0, stride=stride)
+    
+    dX_col = np.zeros_like(X_col)
+    dout_col = dout.transpose(2, 3, 0, 1).ravel()
+
+    #     dX = dpool_fun(dX_col, dout_col, pool_cache)
+    #     def dmaxpool(dX_col, dout_col, pool_cache):
+    max_idx = np.argmax(X_col, axis=0)
+    dX_col[max_idx, range(dout_col.size)] = dout_col
+    #         return dX_col
+
+    dx = col2im_indices(X_col=dX_col, X_shape=(n * d, 1, h, w), field_height=pool_height, 
+                        field_width=pool_width, padding=0, stride=stride)
+    dx = dx.reshape(x.shape)
+
+    #     return dX
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return dx
